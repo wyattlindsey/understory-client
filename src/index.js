9c05c1780dc20334a031f97607e71a79b51e9ed3
wyattlindsey <wyatt@spatially.com>
2018-01-30 21:01:57 -0800

    more scaffolding

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from 'src/components/App'
+
+const Root = props => <App {...props} />
+
+if (typeof window !== 'undefined') {
+  ReactDOM.render(Root(), document.getElementById('root'))
+
+  if (module.hot) {
+    module.hot.accept('components/App', () => {
+      ReactDOM.render(Root(), document.getElementById('root'))
+    })
+  }
+}
+
+export default Root
