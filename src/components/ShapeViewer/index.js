81558a268b55a828a5954dd846b80f1b3154574d
wyattlindsey <wyatt@spatially.com>
2018-05-10 08:28:54 -0700

    use react async component to defer loading of gl view

--- src/components/ShapeViewer/index.js
+++ src/components/ShapeViewer/index.js
@@ -1,1 +1,10 @@
-export { default } from './ShapeViewer'
+import * as React from 'react'
+import { asyncComponent } from 'react-async-component'
+
+const asyncShapeViewer = asyncComponent({
+  LoadingComponent: () => <div>Loading component</div>,
+  name: 'ShapeViewer',
+  resolve: () => System.import('./ShapeViewer'),
+})
+
+export default asyncShapeViewer
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,4 +1,3 @@
-// @flow
 
 import React from 'react'
 import cssModules from 'react-css-modules'
