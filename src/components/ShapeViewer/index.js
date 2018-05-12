fedd0b696f64a16d418502f0d46a841b7ac68e63
wyattlindsey <wyatt@spatially.com>
2018-05-11 21:52:52 -0700

    add a spinner

--- src/components/ShapeViewer/index.js
+++ src/components/ShapeViewer/index.js
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import { asyncComponent } from 'react-async-component'
+import { CircularProgress } from 'material-ui/Progress'
 
 const asyncShapeViewer = asyncComponent({
-  LoadingComponent: () => <div>Loading component</div>,
+  LoadingComponent: () => <CircularProgress />,
   name: 'ShapeViewer',
   resolve: () => System.import('./ShapeViewer'),
+  serverMode: 'defer',
 })
 
 export default asyncShapeViewer
